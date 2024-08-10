c949e6ed3abf7849f38c01a61a0d13f6389bc99e
Charnom <pontogreen1@gmail.com>
2024-08-10 12:47:40 +0700

    Feat: Add edit note function

--- psunote/noteapp.py
+++ psunote/noteapp.py
@@ -1,5 +1,5 @@
 import flask
-
+from flask import request
 import models
 import forms
 
@@ -24,12 +24,10 @@ def index():
         notes=notes,
     )
 
-
 @app.route("/notes/create", methods=["GET", "POST"])
 def notes_create():
     form = forms.NoteForm()
     if not form.validate_on_submit():
-        print("error", form.errors)
         return flask.render_template(
             "notes-create.html",
             form=form,
@@ -56,6 +54,39 @@ def notes_create():
     db.session.commit()
 
     return flask.redirect(flask.url_for("index"))
+
+@app.route("/notes/edit/<note_id>", methods=["GET", "POST"])
+def notes_edit(note_id=None):
+    note = models.Note.query.get(note_id)
+    form = forms.NoteForm(obj=note)
+    if not form.validate_on_submit():
+        return flask.render_template(
+            "notes-create.html",
+            form=form,
+        )
+    note.title = form.title.data
+    note.description = form.description.data
+    note.tags = []
+
+    db = models.db
+    for tag_name in form.tags.data:
+        tag = (
+            db.session.execute(db.select(models.Tag).where(models.Tag.name == tag_name))
+            .scalars()
+            .first()
+        )
+
+        if not tag:
+            tag = models.Tag(name=tag_name)
+            db.session.add(tag)
+
+        note.tags.append(tag)
+
+    db.session.add(note)
+    db.session.commit()
+
+    return flask.redirect(flask.url_for("index"))
+
 
 
 @app.route("/tags/<tag_name>")
